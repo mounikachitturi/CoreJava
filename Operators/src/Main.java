42f8f72475a92f25d3ff6b13c182d0e811bf90db
mounikachitturi <mounikachitturi686@gmail.com>
2023-04-12 15:50:38 +0530

    opreators

---
+++ Operators/src/Main.java
@@ -0,0 +1,52 @@
+import com.sun.jdi.IntegerValue;
+
+public class Main {
+    //   public static void main(String[] args) {System.out.println("Hello world!");}}
+
+    // public static void main(String args[]) {
+//    boolean bo = true;
+//    System.out.println(" boolean values " + bo);
+//    int a = (bo)?1:0;
+//    System.out.println(" boolean to converted into integer value " + bo);
+//   }
+//
+
+
+ //   public static void main(String args[]) {
+   //     int a = 24;
+   //     System.out.println("integer value " + a);
+   //     boolean b1 = true;
+   //     System.out.println(Integer.valueOf(100).equals(b1));
+   //     System.out.println("integer converted into boolean " + b1);
+   // }
+ //  public static void main(String args[]){
+       int a = -100;
+ //      System.out.println("integer value " + a);
+ //      boolean b1;
+ //      if (a >= 1) {
+ //          b1 = true;
+ //      } else {
+ //          b1 = false;
+ //      }
+ //      System.out.println(a + " integer to converted into boolean " + b1);
+
+
+
+    public static void main(String args[]){
+
+        char ch = 's';
+        System.out.println(" character " + ch);
+        int a = 40;
+        System.out.println(Integer.valueOf(50));
+        System.out.println("  character converted into integer : " + ch);
+    }
+
+
+
+
+
+
+}
+
+
+
